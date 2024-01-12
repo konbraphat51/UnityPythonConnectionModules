af870e48da79dbcdd6cd1f6f2d051485cad2f324
konbraphat51 <konbraphat@gmail.com>
2024-01-13 08:09:39 +0900

    prepare events

---
+++ UnitySocket/Assets/Scripts/DataClass.cs
@@ -0,0 +1,4 @@
+/// <summary>
+/// Data for receiving data from Python server
+/// </summary>
+abstract class DataClass { }
--- UnitySocket/Assets/Scripts/DataDecoder.cs
+++ UnitySocket/Assets/Scripts/DataDecoder.cs
@@ -1,15 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 
 abstract class DataDecoder : ScriptableObject
 {
     /// <summary>
-    ///
+    /// Dictionary of data type name to actual C# class type reference
     /// </summary>
-    private Dictionary<Type, UnityEvent<Type>> correspondingEvent = default;
+    private Dictionary<Type, UnityEvent<DataClass>> correspondingEvents;
 
     /// <summary>
     /// Convert from data_type name to actual C# class type reference
@@ -25,5 +26,18 @@ abstract class DataDecoder : ScriptableObject
     {
         //get pre-defined data type
         Dictionary<string, Type> dataToType = DataToType();
+
+        //initialize `correspondingEvents`
+        PrepareEvents(dataToType.Values.ToArray());
+    }
+
+    private void PrepareEvents(Type[] types)
+    {
+        //prepare events
+        correspondingEvents = new Dictionary<Type, UnityEvent<DataClass>>();
+        foreach (Type type in types)
+        {
+            correspondingEvents.Add(type, new UnityEvent<DataClass>());
+        }
     }
 }
