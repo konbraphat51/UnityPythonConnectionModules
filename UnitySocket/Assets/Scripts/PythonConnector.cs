2abe2f4e21a5d9f1ec65b1ed880752f117df6d8d
konbraphat51 <konbraphat@gmail.com>
2024-01-12 18:59:15 +0900

    configure socket variables

--- UnitySocket/Assets/Scripts/PythonConnector.cs
+++ UnitySocket/Assets/Scripts/PythonConnector.cs
@@ -6,11 +6,13 @@ License: Boost Software License (BSL1.0)
 
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 
 /// <summary>
 /// Connecting to Python
 ///
+/// This will be a client socket to connect to Python server
 /// Singleton
 /// </summary>
 public class PythonConnector : MonoBehaviour
@@ -39,4 +41,21 @@ public class PythonConnector : MonoBehaviour
     /// For remember singleton instance
     /// </summary>
     private static PythonConnector _instance;
+
+    [Tooltip("IP Address of Python Server")]
+    [SerializeField]
+    private string ipAddress = "127.0.0.1";
+
+    [Tooltip("Port of Python Server")]
+    [SerializeField]
+    private int portPython = 9000;
+
+    [Tooltip("Port of Unity Server")]
+    [SerializeField]
+    private int portThis = 9001;
+
+    public bool StartConnection()
+    {
+        return true;
+    }
 }
