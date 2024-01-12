d50d96b46809112e66e941db2d84e71507f3ef8b
konbraphat51 <konbraphat@gmail.com>
2024-01-12 18:46:20 +0900

    add comment

--- UnitySocket/Assets/Scripts/PythonConnector.cs
+++ UnitySocket/Assets/Scripts/PythonConnector.cs
@@ -35,5 +35,8 @@ public class PythonConnector : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// For remember singleton instance
+    /// </summary>
     private static PythonConnector _instance;
 }
